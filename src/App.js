8fb705379b33e290e93a8ef9afe31841520a4393
Shuoyuan ZHANG <shuoyuanz@student.unimelb.edu.au>
2023-07-06 10:13:13 +1000

    Create a button to fetch new dogs
    Add a specification

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Header from './components/Header';
+import useDogs from './services/RandomDogs';
+import GenerateButton from './components/Button';
 
 function App() {
+  const { dogs, getAllDogs } = useDogs();
+
   return (
     <div>
       <Header />
+      <main>
+        <GenerateButton getAllDogs={getAllDogs} />
+      </main>
     </div>
   );
 }
---
+++ src/components/Button.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
+
+// display a button and a specification
+const GenerateButton = ({ getAllDogs }) => {
+
+  console.log(getAllDogs);
+
+  // restyled button
+  const LongButton = styled(Button)(({ }) => ({
+    width: '50%',
+  }));
+
+  return (
+    <Container>
+      <Typography
+        fontSize="25px"
+        align="center"
+        color="text.primary"
+        margin="5%"
+      >
+        Click button to generate new dogs
+      </Typography>
+      <Stack
+        direction="row"
+        justifyContent="center"
+        margin="0% 0% 10% 0%"
+      >
+        <LongButton onClick={getAllDogs}
+          variant="contained"
+          size="large"
+        >
+          New Dogs
+        </LongButton>
+      </Stack>
+    </Container>
+  )
+};
+
+export default GenerateButton;
